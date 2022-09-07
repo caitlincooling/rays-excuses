fa81411a8dbbcf12b88e899b7f95fecefe5d7d2c
nadinedelia <nadine.delia@yahoo.com>
2022-09-07 16:58:57 +0100

    new excuse

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.css';
 const picture = require("../assets/rayheader.png");
 
 const Page = () => {
@@ -12,24 +11,30 @@ const Page = () => {
     "I have to work late",
     "Why bother with an excuse? Just don't show up",
     "It's Joe's festival thing in his home town in Somerset",
-    "I have Yoga"
+    "I have Yoga",
+    "I'm probably only going to finish this sid work at like 6.30ish 7ish"
   ];
 
   const generateExcuse = () => {
-    const index = Math.floor(Math.random() * excuses.length)
+    const index = Math.floor(Math.random() * excuses.length);
     alert(excuses[index]);
   };
 
   return (
     <>
-      <img className="picture" src={picture} width="100%" alt="a legend" />
-      <center>
-      <div className="container">
-      <h1>Rays Excuses</h1>
-      <p>Click to generate an excuse to get out of any social event</p>
-      <button onClick={generateExcuse}>Generate</button>
+      <div className="wrapper row ">
+        <div className="col-12">
+          <img className="picture " src={picture} alt="a legend" />
+        </div>
+        <div className="container col-lg-12">
+          <h1>Rays Excuses</h1>
+
+          <p>Click to generate an excuse to get out of any social event</p>
+          <button type="button" onClick={generateExcuse} class="btn btn-light">
+            Generate
+          </button>
+        </div>
       </div>
-      </center>
     </>
   );
 };
